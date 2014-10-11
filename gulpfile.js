b1f44239ce7c0545c201bc25795578c14cbcab00
Roni Laukkarinen <roni@laukkarinen.info>
2014-10-11 14:40:56 +0300

    configs, tests

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,28 @@ var pixrem      = require('gulp-pixrem');
 
 /* 
 
+ERROR HANDLING
+==============
+*/
+
+var handleErrors = function() {
+module.exports = function() {
+
+  var args = Array.prototype.slice.call(arguments);
+
+  // Send error to notification center with gulp-notify
+  notify.onError({
+    title: "Compile Error",
+    message: "<%= error.message %>"
+  }).apply(this, args);
+
+  // Keep gulp from hanging on this task
+  this.emit('end');
+};
+};
+
+/* 
+
 FILE PATHS
 ==========
 */
